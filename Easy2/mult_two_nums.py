6832eaea0c1b8464b1200a7ee33269ad73c87643
JJ Nathan <jjeckertnathan@JJs-MacBook-Pro.local>
2024-07-11 11:56:42 +0200

    11.7.2024 completed Easy 2

---
+++ Easy2/mult_two_nums.py
@@ -0,0 +1,12 @@
+'''
+Create a function that takes two arguments, multiplies them together, and returns the 
+result.
+'''
+
+num1 = int(input("pick a number: ")) 
+num2 = int(input("and multiply it by: "))
+
+def multiply(num1, num2): 
+	print(f'equals {num1 * num2}')
+
+multiply(num1, num2)
