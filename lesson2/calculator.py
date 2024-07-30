852265476a1ee56b468107c7b466f404a67ef381
jerome <jeromee@hawaii.edu>
2024-07-30 17:39:38 +0200

    updated calc.py & .pylintrc files

--- lesson2/calculator.py
+++ lesson2/calculator.py
@@ -1,57 +1,51 @@
 """
-Calculator Module
+Calculator Module 
 """
-# Ask user for two numbers (num1 & num2) 
-# Ask user for an operation to perform
-# Perform operation and print the result to the terminal
+# prompt function
+def prompt(message):
+    print(f"==> {message}")
 
-#Define function for prompt output
-def prompt(message): 
-    print(f'==> {message}')
-
-def invalid_num(number_str): 
+# deal with invalid input
+def invalid_number(number_str):
     try:
-        float(number_str)
+        int(number_str)
     except ValueError:
         return True
+
     return False
 
 prompt('Welcome to Calculator!')
 
 prompt("What's the first number?")
-num1 = input()
+number1 = input()
 
-while invalid_num(num1):
+while invalid_number(number1):
     prompt("Hmm... that doesn't look like a valid number.")
-    num1 = input()
+    number1 = input()
 
 prompt("What's the second number?")
-num2 = input()
+number2 = input()
 
-while invalid_num(num2):
+while invalid_number(number2):
     prompt("Hmm... that doesn't look like a valid number.")
-    num2 = input()
+    number2 = input()
 
-prompt("What operation would you like to perform?\n1. Add\n2. Subtract\n" 
-"3. Multiply\n4. Divide")
+prompt("""What operation would you like to perform?
+       \n1) Add 2) Subtract 3) Multiply 4) Divide""")
 operation = input()
 
 while operation not in ["1", "2", "3", "4"]:
     prompt("You must choose 1, 2, 3, or 4")
     operation = input()
 
-num1 = float(num1)
-num2 = float(num2)
-
 match operation:
-    case '1':
-        output = num1 + num2
-    case '2':
-        output = num1 - num2
-    case '3':
-        output = num1 * num2
-    case '4':
-        output = num1 / num2
+    case "1":
+        output = int(number1) + int(number2)
+    case "2":
+        output = int(number1) - int(number2)
+    case "3":
+        output = int(number1) * int(number2)
+    case "4":
+        output = int(number1) / int(number2)
 
-print(f"==> Result: {output}")
-
+prompt(f"The result is {output}")
