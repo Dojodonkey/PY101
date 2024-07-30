8f3fedb1468b0ebd45ad8b5bb09681242feda72e
jerome <jeromee@hawaii.edu>
2024-07-30 16:41:05 +0200

    env packages

---
+++ lesson2/calculator.py
@@ -0,0 +1,57 @@
+"""
+Calculator Module
+"""
+# Ask user for two numbers (num1 & num2) 
+# Ask user for an operation to perform
+# Perform operation and print the result to the terminal
+
+#Define function for prompt output
+def prompt(message): 
+    print(f'==> {message}')
+
+def invalid_num(number_str): 
+    try:
+        float(number_str)
+    except ValueError:
+        return True
+    return False
+
+prompt('Welcome to Calculator!')
+
+prompt("What's the first number?")
+num1 = input()
+
+while invalid_num(num1):
+    prompt("Hmm... that doesn't look like a valid number.")
+    num1 = input()
+
+prompt("What's the second number?")
+num2 = input()
+
+while invalid_num(num2):
+    prompt("Hmm... that doesn't look like a valid number.")
+    num2 = input()
+
+prompt("What operation would you like to perform?\n1. Add\n2. Subtract\n" 
+"3. Multiply\n4. Divide")
+operation = input()
+
+while operation not in ["1", "2", "3", "4"]:
+    prompt("You must choose 1, 2, 3, or 4")
+    operation = input()
+
+num1 = float(num1)
+num2 = float(num2)
+
+match operation:
+    case '1':
+        output = num1 + num2
+    case '2':
+        output = num1 - num2
+    case '3':
+        output = num1 * num2
+    case '4':
+        output = num1 / num2
+
+print(f"==> Result: {output}")
+
